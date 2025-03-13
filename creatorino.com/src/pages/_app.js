c5992e7e3d9e9e1b131831be5ea4e8d1e3569087
Benjamin McDaniel <benjamin.f.mcdaniel@gmail.com>
2025-03-13 01:32:40 -0700

    x

--- creatorino.com/src/pages/_app.js
+++ creatorino.com/src/pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { useEffect } from 'react';
+import Head from 'next/head';  // Make sure this import is here
 import { supabase } from '../lib/supabaseClient';
 import theme from '../utils/theme';
 import '../styles/globals.css'; // For Tailwind CSS
@@ -38,7 +39,6 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <ThemeProvider theme={theme}>
